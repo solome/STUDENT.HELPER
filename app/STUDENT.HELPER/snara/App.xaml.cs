efae2077ee4fd43c657823628fbd8e765e7b75c3
失的猛 <sunshine1914@live.cn>
2013-05-19 23:24:07 +0800

    更新项目
    创建一个新项目，重新开始；主要是添加新的功能。而之前的项目框架并不适合该新功能；如markdown的支持！

---
+++ app/STUDENT.HELPER/snara/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using snara.Common;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace snara
+{
+
+    sealed partial class App : Application
+    {
+
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        protected override async void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                                    
+                SuspensionManager.RegisterFrame(rootFrame, "AppFrame");
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    try
+                    {
+                        await SuspensionManager.RestoreAsync();
+                    }
+                    catch (SuspensionManagerException)
+                    {
+                    }
+                }
+
+                Window.Current.Content = rootFrame;
+            }
+            if (rootFrame.Content == null)
+            {
+                if (!rootFrame.Navigate(typeof(ItemsPage), "AllGroups"))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            Window.Current.Activate();
+        }
+
+        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await SuspensionManager.SaveAsync();
+            deferral.Complete();
+        }
+    }
+}
---
+++ app/STUDENT.HELPER/snara/SplitPage.xaml.cs
@@ -0,0 +1,144 @@
+﻿using snara.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Split Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234234
+
+namespace snara
+{
+    /// <summary>
+    /// A page that displays a group title, a list of items within the group, and details for the
+    /// currently selected item.
+    /// </summary>
+    public sealed partial class SplitPage : snara.Common.LayoutAwarePage
+    {
+        public SplitPage()
+        {
+            this.InitializeComponent();
+        }
+
+        #region Page state management
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+            var group = SampleDataSource.GetGroup((String)navigationParameter);
+            this.DefaultViewModel["Group"] = group;
+            this.DefaultViewModel["Items"] = group.Items;
+
+            if (pageState == null)
+            {
+                this.itemListView.SelectedItem = null;
+                // When this is a new page, select the first item automatically unless logical page
+                // navigation is being used (see the logical page navigation #region below.)
+                if (!this.UsingLogicalPageNavigation() && this.itemsViewSource.View != null)
+                {
+                    this.itemsViewSource.View.MoveCurrentToFirst();
+                }
+            }
+            else
+            {
+                // Restore the previously saved state associated with this page
+                if (pageState.ContainsKey("SelectedItem") && this.itemsViewSource.View != null)
+                {
+                    var selectedItem = SampleDataSource.GetItem((String)pageState["SelectedItem"]);
+                    this.itemsViewSource.View.MoveCurrentTo(selectedItem);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+            if (this.itemsViewSource.View != null)
+            {
+                var selectedItem = (SampleDataItem)this.itemsViewSource.View.CurrentItem;
+                if (selectedItem != null) pageState["SelectedItem"] = selectedItem.UniqueId;
+            }
+        }
+
+        #endregion
+
+        #region Logical page navigation
+
+        private bool UsingLogicalPageNavigation(ApplicationViewState? viewState = null)
+        {
+            if (viewState == null) viewState = ApplicationView.Value;
+            return viewState == ApplicationViewState.FullScreenPortrait ||
+                viewState == ApplicationViewState.Snapped;
+        }
+
+        void ItemListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+    
+            if (this.UsingLogicalPageNavigation()) this.InvalidateVisualState();
+        }
+
+        protected override void GoBack(object sender, RoutedEventArgs e)
+        {
+            if (this.UsingLogicalPageNavigation() && itemListView.SelectedItem != null)
+            {
+                this.itemListView.SelectedItem = null;
+            }
+            else
+            {
+                base.GoBack(sender, e);
+            }
+        }
+
+        protected override string DetermineVisualState(ApplicationViewState viewState)
+        {
+            // Update the back button's enabled state when the view state changes
+            var logicalPageBack = this.UsingLogicalPageNavigation(viewState) && this.itemListView.SelectedItem != null;
+            var physicalPageBack = this.Frame != null && this.Frame.CanGoBack;
+            this.DefaultViewModel["CanGoBack"] = logicalPageBack || physicalPageBack;
+
+            // Determine visual states for landscape layouts based not on the view state, but
+            // on the width of the window.  This page has one layout that is appropriate for
+            // 1366 virtual pixels or wider, and another for narrower displays or when a snapped
+            // application reduces the horizontal space available to less than 1366.
+            if (viewState == ApplicationViewState.Filled ||
+                viewState == ApplicationViewState.FullScreenLandscape)
+            {
+                var windowWidth = Window.Current.Bounds.Width;
+                if (windowWidth >= 1366) return "FullScreenLandscapeOrWide";
+                return "FilledOrNarrow";
+            }
+
+            // When in portrait or snapped start with the default visual state name, then add a
+            // suffix when viewing details instead of the list
+            var defaultStateName = base.DetermineVisualState(viewState);
+            return logicalPageBack ? defaultStateName + "_Detail" : defaultStateName;
+        }
+
+        #endregion
+    }
+}
